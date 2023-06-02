aded0c144f9b4b27109d1376bb4a6ddc7c76a2d1
Edward Baldacchino <edwardjbaldacchino@gmail.com>
2023-06-02 21:10:49 +1000

    Added Spinner component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export * from "./Button";
 export * from "./FormControl";
 export * from "./Input";
 export * from "./Portal";
+export * from "./Spinner";
 export * from "./TextField";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/500.css";
