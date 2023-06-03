4260125b4c56edbadaa9179d02620a069933ee51
Edward Baldacchino <edwardjbaldacchino@gmail.com>
2023-06-03 17:40:47 +1000

    Added Tooltip component

--- packages/styled/index.ts
+++ packages/styled/index.ts
@@ -5,6 +5,7 @@ export * from "./FormControl";
 export * from "./Input";
 export * from "./Spinner";
 export * from "./TextField";
+export * from "./Tooltip";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/500.css";
 import "@fontsource/open-sans/700.css";
