0327c8df1505100cd51f082ed853bb072ac3bafd
Edward Baldacchino <edwardjbaldacchino@gmail.com>
2024-06-05 08:02:59 +1000

    Setup build scripts for packages

---
+++ packages/styled/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	build: {
+		sourcemap: true,
+		lib: {
+			entry: path.resolve(__dirname, "index.ts"),
+			name: "@bui/styled",
+		},
+		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: ["react", "react-dom"],
+			output: {
+				// Provide global variables to use in the UMD build
+				// for externalized deps
+				globals: {
+					react: "React",
+					"react-dom": "ReactDOM"
+				},
+			},
+		},
+	},
+});
